5ee50ce6b7cfba24f16e1d5d209dfbf6055fa6f6
MacBook Boba <alfakhri1998@gmail.com>
2020-07-10 23:18:48 +0700

    updat directory path local

--- lib/domain/usecase/aws_store.dart
+++ lib/domain/usecase/aws_store.dart
@@ -206,14 +206,16 @@ class AwsStore {
       newKecAngin = int.parse(kecAngin.split('.')[0]);
       newKecAngin = newKecAngin + 1;
       if (newKecAngin.toString().length == 1) {
-        newKecAngin = int.parse(StringUtils.addCharAtPosition(newKecAngin.toString(), "0", 0));
-      } 
+        newKecAngin = int.parse(
+            StringUtils.addCharAtPosition(newKecAngin.toString(), "0", 0));
+      }
       print("Kecepatan Angin: " + newKecAngin.toString());
     } else {
       newKecAngin = int.parse(kecAngin);
       if (newKecAngin.toString().length == 1) {
-        newKecAngin = int.parse(StringUtils.addCharAtPosition(newKecAngin.toString(), "0", 0));
-      } 
+        newKecAngin = int.parse(
+            StringUtils.addCharAtPosition(newKecAngin.toString(), "0", 0));
+      }
       print("Kecepatan Angin: " + newKecAngin.toString());
     }
 
@@ -249,7 +251,7 @@ class AwsStore {
     var newTinggiHshs1;
     newTinggiHshs1 =
         int.parse(tinggiHshs1) / 30; //Konversi string kedalam integer
-    
+
     //Tinggi hshs 2
     var newTinggiHshs2;
     newTinggiHshs2 = int.parse(tinggiHshs2) / 30;
@@ -446,19 +448,18 @@ class AwsStore {
         newRadiasi = StringUtils.addCharAtPosition(newRadiasi, "00", 0);
       } else if (newRadiasi.length == 3) {
         newRadiasi = StringUtils.addCharAtPosition(newRadiasi, "0", 0);
-      } else if (newRadiasi  == "0"){
-      newRadiasi = "0000";
+      } else if (newRadiasi == "0") {
+        newRadiasi = "0000";
       }
     } else {
       if (newRadiasi.length == 2) {
         newRadiasi = StringUtils.addCharAtPosition(newRadiasi, "00", 0);
       } else if (newRadiasi.length == 3) {
         newRadiasi = StringUtils.addCharAtPosition(newRadiasi, "0", 0);
-      } else if (newRadiasi  == "0"){
-      newRadiasi = "0000";
+      } else if (newRadiasi == "0") {
+        newRadiasi = "0000";
       }
     }
-    
 
     //Penyinaran matahari
     String newPenyinaranMatahari;
@@ -503,21 +504,128 @@ class AwsStore {
         arahCL1.split("-")[0] +
         arahAwanMenengah.split("-")[0] +
         arahAwanTinggi.split("-")[0];
-    _sandi20 = "57" + jenisAwanKonvektif.split("-")[0] + arahAwanKonvektif.split("-")[0] + sudutAwanKonvektif.split("-")[0];
+    _sandi20 = "57" +
+        jenisAwanKonvektif.split("-")[0] +
+        arahAwanKonvektif.split("-")[0] +
+        sudutAwanKonvektif.split("-")[0];
     _sandi21 = "58" + newSelisihQFF;
     _sandi22 = "6" + newHujan3jamlalu + "7";
     _sandi23 = _sandi20 = "8" +
         banyakOktaNS1.split("-")[0] +
         jenisC1.split("-")[0] +
         newTinggiHshs1.toString().split(".")[0];
-    _sandi24 = "8" + banyakOktaNS2.split("-")[0] + jenisC2.split("-")[0] + newTinggiHshs2.toString().split(".")[0];
-    _sandi25 = "8" + banyakOktaNS3.split("-")[0] + jenisC3.split("-")[0] + newTinggiHshs3.toString().split(".")[0];
+    _sandi24 = "8" +
+        banyakOktaNS2.split("-")[0] +
+        jenisC2.split("-")[0] +
+        newTinggiHshs2.toString().split(".")[0];
+    _sandi25 = "8" +
+        banyakOktaNS3.split("-")[0] +
+        jenisC3.split("-")[0] +
+        newTinggiHshs3.toString().split(".")[0];
 
-    print("1. $sandiPengenal 2. $_sandi2 3. $nomorStasiun 4. $_sandi4 5. $_sandi5 6. $_sandi6 7. $_sandi7 8. $_sandi8 9. $_sandi9 10. $_sandi10 11. $_sandi11 12. $_sandi12 13. $_sandi13 14. $_sandi14 15. $_sandi15 16. $_sandi16 17. $_sandi17 18. $_sandi18 19. $_sandi19 20. $_sandi20 21. $_sandi21 22. $_sandi22 23. $_sandi23 24. $_sandi24 25. $_sandi25");
-    _hasilSandi48 = sandiPengenal + " " + _sandi2 + " " + nomorStasiun + " " + _sandi4 + " " + _sandi5 + " " + _sandi6 + " " + _sandi7 + " " + _sandi8 + " " + _sandi9 + " " + _sandi10 + " " + _sandi11 + " " + _sandi12 + " " + _sandi13 + " " + _sandi14 + " " + _sandi15 + " " +  _sandi16 + " " + _sandi17 + " " + _sandi18 + " " + _sandi19 + " " + _sandi20 + " " + _sandi21 + " " + _sandi22 + " " + _sandi23 + " " + _sandi24 + " " + _sandi25;
+    print(
+        "1. $sandiPengenal 2. $_sandi2 3. $nomorStasiun 4. $_sandi4 5. $_sandi5 6. $_sandi6 7. $_sandi7 8. $_sandi8 9. $_sandi9 10. $_sandi10 11. $_sandi11 12. $_sandi12 13. $_sandi13 14. $_sandi14 15. $_sandi15 16. $_sandi16 17. $_sandi17 18. $_sandi18 19. $_sandi19 20. $_sandi20 21. $_sandi21 22. $_sandi22 23. $_sandi23 24. $_sandi24 25. $_sandi25");
+    _hasilSandi48 = sandiPengenal +
+        " " +
+        _sandi2 +
+        " " +
+        nomorStasiun +
+        " " +
+        _sandi4 +
+        " " +
+        _sandi5 +
+        " " +
+        _sandi6 +
+        " " +
+        _sandi7 +
+        " " +
+        _sandi8 +
+        " " +
+        _sandi9 +
+        " " +
+        _sandi10 +
+        " " +
+        _sandi11 +
+        " " +
+        _sandi12 +
+        " " +
+        _sandi13 +
+        " " +
+        _sandi14 +
+        " " +
+        _sandi15 +
+        " " +
+        _sandi16 +
+        " " +
+        _sandi17 +
+        " " +
+        _sandi18 +
+        " " +
+        _sandi19 +
+        " " +
+        _sandi20 +
+        " " +
+        _sandi21 +
+        " " +
+        _sandi22 +
+        " " +
+        _sandi23 +
+        " " +
+        _sandi24 +
+        " " +
+        _sandi25;
 
-    print("1. $newStasiun 2. $newTanggal45 3. $newTime45 4. $newTitikEmbun 5. ${bagianLangit.split('-')[0]} 6. $newArahAngin 7. $newKecAngin 8. $penglihatanMendatar 9. ${keadaanCuacaWaktuPengamatan.split("-")[0]} 10. ${keadaanCuacaLalu1.split("-")[0]} 11. ${keadaanCuacaLalu2.split("-")[0]} 12. $newQFF 13. $newBolaKering 14. ${banyakOktaNH.split("-")[0]} 15. ${awanRendahJenis.split("-")[0]} 16. ${tinggiDasarCL1.split("-")[0]} 17. ${awanMenengahJenis.split("-")[0]} 18. ${awanTinggiJenis.split("-")[0]} 19. ${banyakOktaNS1.split("-")[0]} 20. ${jenisC1.split("-")[0]} 21. ${newTinggiHshs1.toString().split(".")[0]} 22. ${banyakOktaNS2.split("-")[0]} 23. ${jenisC2.split("-")[0]} 24. ${newTinggiHshs2.toString().split(".")[0]} 25. 0 26. ${jenisC3.split("-")[0]} 27. ${newTinggiHshs3.toString().split(".")[0]} 28. ${jenisAwanKonvektif.split("-")[0]} 29. ${arahAwanKonvektif.split("-")[0]} 30. ${sudutAwanKonvektif.split("-")[0]} 31. ${lembabNisbi.split(".")[0]} 32. $newQFE 33. $newBolaBasah 34. $newHujan6jamlalu 35. 1 36. ${suhuMax.replaceAll(".", "")} 37. ${suhuMin.replaceAll(".", "")} 38. $newJumlahPenguapan24jam 39. $newRadiasi 40. $newPenyinaranMatahari 41. ${arahCL1.split("-")[0]} 42. ${arahAwanMenengah.split("-")[0]} 43. ${arahAwanTinggi.split("-")[0]} 44. $newSelisihQFF 45. ${pengenalDataAngin.split("-")[0]} 46. ${pengenalDataCuaca.split("-")[0]} 47. ${pengenalDataHujan.split("-")[0]} 48. ${dataAlatPenguapan.split("-")[0]}");
-    _hasilSandi45 = newStasiun + newTanggal45 + "\n" + newTime45 + newTitikEmbun + bagianLangit.split('-')[0] + newArahAngin + newKecAngin.toString() + penglihatanMendatar + keadaanCuacaWaktuPengamatan.split("-")[0] + keadaanCuacaLalu1.split("-")[0] + keadaanCuacaLalu2.split("-")[0] + newQFF + newBolaKering + banyakOktaNH.split("-")[0] + awanRendahJenis.split("-")[0] + tinggiDasarCL1.split("-")[0] + awanMenengahJenis.split("-")[0] + awanTinggiJenis.split("-")[0] + banyakOktaNS1.split("-")[0] + jenisC1.split("-")[0] + newTinggiHshs1.toString().split(".")[0] + banyakOktaNS2.split("-")[0] + jenisC2.split("-")[0] + newTinggiHshs2.toString().split(".")[0] + "0" + jenisC3.split("-")[0] + newTinggiHshs3.toString().split(".")[0] + jenisAwanKonvektif.split("-")[0] + arahAwanKonvektif.split("-")[0] + sudutAwanKonvektif.split("-")[0] + lembabNisbi.split(".")[0] + newQFE + newBolaBasah + newHujan6jamlalu + "1" + suhuMax.replaceAll(".", "") + suhuMin.replaceAll(".", "") + newJumlahPenguapan24jam + newRadiasi + newPenyinaranMatahari + arahCL1.split("-")[0] + arahAwanMenengah.split("-")[0] + arahAwanTinggi.split("-")[0] + newSelisihQFF + pengenalDataAngin.split("-")[0] + pengenalDataCuaca.split("-")[0] + pengenalDataHujan.split("-")[0] + dataAlatPenguapan.split("-")[0];
+    print(
+        "1. $newStasiun 2. $newTanggal45 3. $newTime45 4. $newTitikEmbun 5. ${bagianLangit.split('-')[0]} 6. $newArahAngin 7. $newKecAngin 8. $penglihatanMendatar 9. ${keadaanCuacaWaktuPengamatan.split("-")[0]} 10. ${keadaanCuacaLalu1.split("-")[0]} 11. ${keadaanCuacaLalu2.split("-")[0]} 12. $newQFF 13. $newBolaKering 14. ${banyakOktaNH.split("-")[0]} 15. ${awanRendahJenis.split("-")[0]} 16. ${tinggiDasarCL1.split("-")[0]} 17. ${awanMenengahJenis.split("-")[0]} 18. ${awanTinggiJenis.split("-")[0]} 19. ${banyakOktaNS1.split("-")[0]} 20. ${jenisC1.split("-")[0]} 21. ${newTinggiHshs1.toString().split(".")[0]} 22. ${banyakOktaNS2.split("-")[0]} 23. ${jenisC2.split("-")[0]} 24. ${newTinggiHshs2.toString().split(".")[0]} 25. 0 26. ${jenisC3.split("-")[0]} 27. ${newTinggiHshs3.toString().split(".")[0]} 28. ${jenisAwanKonvektif.split("-")[0]} 29. ${arahAwanKonvektif.split("-")[0]} 30. ${sudutAwanKonvektif.split("-")[0]} 31. ${lembabNisbi.split(".")[0]} 32. $newQFE 33. $newBolaBasah 34. $newHujan6jamlalu 35. 1 36. ${suhuMax.replaceAll(".", "")} 37. ${suhuMin.replaceAll(".", "")} 38. $newJumlahPenguapan24jam 39. $newRadiasi 40. $newPenyinaranMatahari 41. ${arahCL1.split("-")[0]} 42. ${arahAwanMenengah.split("-")[0]} 43. ${arahAwanTinggi.split("-")[0]} 44. $newSelisihQFF 45. ${pengenalDataAngin.split("-")[0]} 46. ${pengenalDataCuaca.split("-")[0]} 47. ${pengenalDataHujan.split("-")[0]} 48. ${dataAlatPenguapan.split("-")[0]}");
+    _hasilSandi45 = newStasiun +
+        newTanggal45 +
+        "\n" +
+        newTime45 +
+        newTitikEmbun +
+        bagianLangit.split('-')[0] +
+        newArahAngin +
+        newKecAngin.toString() +
+        penglihatanMendatar +
+        keadaanCuacaWaktuPengamatan.split("-")[0] +
+        keadaanCuacaLalu1.split("-")[0] +
+        keadaanCuacaLalu2.split("-")[0] +
+        newQFF +
+        newBolaKering +
+        banyakOktaNH.split("-")[0] +
+        awanRendahJenis.split("-")[0] +
+        tinggiDasarCL1.split("-")[0] +
+        awanMenengahJenis.split("-")[0] +
+        awanTinggiJenis.split("-")[0] +
+        banyakOktaNS1.split("-")[0] +
+        jenisC1.split("-")[0] +
+        newTinggiHshs1.toString().split(".")[0] +
+        banyakOktaNS2.split("-")[0] +
+        jenisC2.split("-")[0] +
+        newTinggiHshs2.toString().split(".")[0] +
+        "0" +
+        jenisC3.split("-")[0] +
+        newTinggiHshs3.toString().split(".")[0] +
+        jenisAwanKonvektif.split("-")[0] +
+        arahAwanKonvektif.split("-")[0] +
+        sudutAwanKonvektif.split("-")[0] +
+        lembabNisbi.split(".")[0] +
+        newQFE +
+        newBolaBasah +
+        newHujan6jamlalu +
+        "1" +
+        suhuMax.replaceAll(".", "") +
+        suhuMin.replaceAll(".", "") +
+        newJumlahPenguapan24jam +
+        newRadiasi +
+        newPenyinaranMatahari +
+        arahCL1.split("-")[0] +
+        arahAwanMenengah.split("-")[0] +
+        arahAwanTinggi.split("-")[0] +
+        newSelisihQFF +
+        pengenalDataAngin.split("-")[0] +
+        pengenalDataCuaca.split("-")[0] +
+        pengenalDataHujan.split("-")[0] +
+        dataAlatPenguapan.split("-")[0];
     print("Hasil sandi 45 : $_hasilSandi45");
   }
 
@@ -536,10 +644,13 @@ class AwsStore {
 
   void saveDataToLocal(var data) async {
     var rand = new Random();
+    DateTime now = DateTime.now();
+
+    String formattedDate = Constants().dateFormat.format(now);
 
     try {
       final Directory directory = await getExternalStorageDirectory();
-      final File file = File('${directory.path}/data.txt');
+      final File file = File('${directory.path}/File AWS/$formattedDate/$formattedDate-${rand.nextInt(1000)}.txt');
       await file.writeAsString(jsonEncode(data));
       readData();
     } catch (e) {
